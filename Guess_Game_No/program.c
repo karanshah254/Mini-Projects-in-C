10109356dba341448355958ba5d75f49d5833e2b
Karan Shah <karankshah2504@gmail.com>
2023-02-23 23:25:13 +0530

    Guess The Number & Display The Final Score Of User & Computer

---
+++ Guess_Game_No/program.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+void number();
+
+
+int main()
+
+{
+    int n,us;
+    int comp_nu;
+    int count=0,count1=0;
+
+    printf("\nEnter Limit of Guessing :  ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        printf("\nEnter User's Number : ");
+        scanf("%d",&us);
+
+        comp_nu=rand()%100 +1;
+
+        printf("\nComputer's Selected Number --> %d",comp_nu);
+
+        if(us==comp_nu)
+        {
+            printf("\n|---User Win---|");
+            count+=1;
+        }
+        else{
+            printf("\n|---Computer Win---|");
+            count1+=1;
+        }
+
+
+    }
+    printf("\n-- User Score-- |-- Computer Score --|");
+    printf("\n       %d       |          %d",count,count1);
+}
